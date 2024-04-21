554182a2a80ec838bcd2c9ef12a2c5c573eaa98c
juanex2001 <juanseg201@gmail.com>
2024-04-20 22:04:12 -0500

    Update

--- myapp/Myapp.ice
+++ myapp/Myapp.ice
@@ -1,6 +1,7 @@
 module Demo
 {
      sequence<int> IntList;
+     sequence<IntList> SequenceOfIntLists;
     interface Order
     {
         void sortList(IntList numbers, int numServers);
--- myapp/server/src/main/java/MyappI.java
+++ myapp/server/src/main/java/MyappI.java
@@ -5,30 +5,33 @@ import java.util.concurrent.*;
 
 
 public class MyappI implements Demo.Order {
-
-    public void sortList(List<Integer> numbers, int numServers, Current current) {
-        int listSize = numbers.size();
+    
+    public void sortList(int[] numbers, int numServers, Current current) {
+        int listSize = numbers.length;
         int sublistSize = listSize / numServers;
-
+    
         // Create an ExecutorService with a fixed thread pool size
         ExecutorService executor = Executors.newFixedThreadPool(numServers);
         List<Future<List<Integer>>> futures = new ArrayList<>();
-
+    
         // Distribute workload among connected clients
         for (int i = 0; i < numServers; i++) {
             final int startIndex = i * sublistSize;
             final int endIndex = Math.min((i + 1) * sublistSize, listSize);
-
+    
             // Submit a sorting task to the executor
             Future<List<Integer>> future = executor.submit(() -> {
-                List<Integer> sublist = new ArrayList<>(numbers.subList(startIndex, endIndex));
+                List<Integer> sublist = new ArrayList<>();
+                for (int j = startIndex; j < endIndex; j++) {
+                    sublist.add(numbers[j]);
+                }
                 Collections.sort(sublist); // Sort the sublist
                 return sublist;
             });
-
+    
             futures.add(future); // Store the Future object for later retrieval
         }
-
+    
         // Collect sorted sublists
         List<List<Integer>> sortedSublists = new ArrayList<>();
         for (Future<List<Integer>> future : futures) {
@@ -38,18 +41,20 @@ public class MyappI implements Demo.Order {
                 e.printStackTrace();
             }
         }
-
+    
         // Shutdown the ExecutorService
         executor.shutdown();
-
-        //sortFinalList(sortedSublists)
+    
+        // Combine and sort the final list
+        List<Integer> sortedNumbers = new ArrayList<>();
+        for (List<Integer> sublist : sortedSublists) {
+            sortedNumbers.addAll(sublist);
+        }
+        Collections.sort(sortedNumbers);
+        // Use 'sortedNumbers' as the sorted result
     }
 
-    @Override
-    public void sortList(int[] numbers, int numServers, Current current) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sortList'");
-    }
+   
 
     // public void sortFinalList(List<List<Integer>> numbers){ //compares first value and adds smallest to list until all numbers are in 1 list}
 
