0821e2b35a795620410a19a2a0f460a6ed2a9451
Juan Felipe Jojoa Crespo <juanfejojoa@gmail.com>
2024-04-23 20:44:22 -0500

    JFJC/feat: Improvement on class settings, need to fix imports

--- myapp/client/src/main/java/Client.java
+++ myapp/client/src/main/java/Client.java
@@ -1,9 +1,11 @@
-import java.util.Random;
+import settings.src.main.java.Settings;
 
 public class Client {
     public static void main(String[] args) {
+        Settings systemConfig = new Settings(3, 10000); // Configurar el número de nodos y el puerto
+
         try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args)) {
-            com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimpleOrder: tcp -h localhost -p 10000");
+            com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimpleOrder: tcp -h localhost -p " + systemConfig.getPort());
             Demo.OrderPrx order = Demo.OrderPrx.checkedCast(base);
             if(order == null) {
                 throw new Error("Invalid proxy");
@@ -11,7 +13,7 @@ public class Client {
             Random random = new Random();
             int[] numbers = random.ints(20).toArray();
 
-            order.sortList(numbers, 2);
+            order.sortList(numbers, systemConfig.getNumNodes());
             
         }
     }
--- myapp/server/src/main/java/Server.java
+++ myapp/server/src/main/java/Server.java
@@ -1,8 +1,13 @@
+import settings.src.main.java.Settings;
 public class Server {
     public static void main(String[] args) {
-        int TCPPORT = 10000;
+        int numNodes = 3; // Configurar el número de nodos
+        int port = 10000; // Configurar el puerto
+        
+        Settings systemConfig = new Settings(numNodes, port);
+
         try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args)) {
-            com.zeroc.Ice.ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints("SimpleOrderAdapter", "tcp -h localhost -p " + TCPPORT);
+            com.zeroc.Ice.ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints("SimpleOrderAdapter", "tcp -h localhost -p " + systemConfig.getPort());
             com.zeroc.Ice.Object object = new MyappI();
             adapter.add(object, com.zeroc.Ice.Util.stringToIdentity("SimpleOrder"));
             adapter.activate();
