b1e7331be05eed954eced6442fc42607ed1c3631
juanex2001 <juanseg201@gmail.com>
2024-04-29 15:54:50 -0500

    fix: MergeSort Build

--- SortApp/server/src/main/java/SortFileI.java
+++ SortApp/server/src/main/java/SortFileI.java
@@ -85,39 +85,44 @@ public class SortFileI implements SortFile {
     }
 
     
-    private void mergeSort(List<String> arr) {
-        if (arr.size() <= 1) {
-            return;
+    private String[] mergeSort(String[] arr) {
+        if (arr.length <= 1) {
+            return null;
         }
 
-        int mid = arr.size() / 2;
-        List<String> leftHalf = arr.subList(0, mid);
-        List<String> rightHalf = arr.subList(mid, arr.size());
+        int mid = arr.length / 2;
+        String[] leftHalf = new String[mid];
+        String[] rightHalf = new String[arr.length - mid];
+        System.arraycopy(arr, 0, leftHalf, 0, mid);
+        System.arraycopy(arr, mid, rightHalf, 0, arr.length - mid);
 
         mergeSort(leftHalf);
         mergeSort(rightHalf);
 
-        merge(arr, leftHalf, rightHalf);
+        return merge(arr, leftHalf, rightHalf);
+
     }
 
-    private void merge(List<String> arr, List<String> left, List<String> right) {
+    private String[] merge(String[] arr, String[] left, String[] right) {
         int leftIndex = 0, rightIndex = 0, arrIndex = 0;
 
-        while (leftIndex < left.size() && rightIndex < right.size()) {
-            if (left.get(leftIndex).compareTo(right.get(rightIndex)) < 0) {
-                arr.set(arrIndex++, left.get(leftIndex++));
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex].compareTo(right[rightIndex]) < 0) {
+                arr[arrIndex++] = left[leftIndex++];
             } else {
-                arr.set(arrIndex++, right.get(rightIndex++));
+                arr[arrIndex++] = right[rightIndex++];
             }
         }
 
-        while (leftIndex < left.size()) {
-            arr.set(arrIndex++, left.get(leftIndex++));
+        while (leftIndex < left.length) {
+            arr[arrIndex++] = left[leftIndex++];
         }
 
-        while (rightIndex < right.size()) {
-            arr.set(arrIndex++, right.get(rightIndex++));
+        while (rightIndex < right.length) {
+            arr[arrIndex++] = right[rightIndex++];
         }
+
+        return arr;
     }
 
 
